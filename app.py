2761c5e6a4d0d07f4eac082d1487796bb6b7b5c4
Amanda <amanda.hamynen@gmail.com>
2022-01-25 17:14:25 +0200

    Added more dependencies and modified app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
+import getpass
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
+import re
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+uri = os.getenv("DATABASE_URL")
+if uri is None:
+    uri = f"postgresql:///{getpass.getuser()}"
+elif uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 import routes
